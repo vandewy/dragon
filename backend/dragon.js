a42c6ee88a16e5153f4ce17f512bfc7214ba0ce6
vandy <vandewynkel@gmail.com>
2018-12-31 17:49:23 -0600

    n/a

---
+++ backend/dragon.js
@@ -0,0 +1,8 @@
+class Dragon{
+  constructor(birthdate, nickname){
+    this.birthdate = birthdate;
+    this.nickname = nickname;
+  }
+}
+
+module.exports = Dragon;
---
+++ backend/index.js
@@ -0,0 +1,7 @@
+const Dragon = require('./dragon.js');
+
+const fooey = new Dragon(new Date(), 'fooey');
+const baloo = new Dragon(new Date(), 'baloo');
+
+console.log('fooey', fooey);
+console.log('baloo', baloo);
